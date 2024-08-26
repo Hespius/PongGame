c31318f0e07f811e6135198af5100ad6107e009d
Vinicius <especoto.vini@gmail.com>
2024-08-26 14:43:27 -0300

    feat: Add scores

--- src/main.py
+++ src/main.py
@@ -18,6 +18,22 @@ pygame.display.set_caption(title="Pong")
 # Set up the clock
 clock = pygame.time.Clock()
 
+
+class Score:
+    def __init__(self, position_x, position_y, color, font_size, font) -> None:
+        self.position_x = position_x
+        self.position_y = position_y
+        self.color = color
+        self.font = pygame.font.Font(font, int(font_size))
+        self.score = 0
+
+    def draw(self, surface, player):
+        text = self.font.render(f"{str(player).upper()} : {str(self.score).upper()}", True, self.color)
+        surface.blit(text, (self.position_x, self.position_y))
+
+    def increase(self):
+        self.score += 1
+
 class Paddle:
     def __init__(self, width, height, position_x, position_y, color) -> None:
         self.rect = pygame.Rect(position_x, position_y, width, height)
@@ -104,7 +120,11 @@ player_paddle = Paddle(width=LENGTH/64, height=WIDTH/4, position_x=LENGTH/64, po
 enemy_paddle = Paddle(width=LENGTH/64, height=WIDTH/4, position_x=LENGTH - LENGTH/32, position_y=WIDTH/2, color=(255, 255, 255))
 # Set up the ball
 ball_direction = random.choice([1, -1])
-ball = Ball(radius=LENGTH/64, position_x=LENGTH/2 - LENGTH/64, position_y=WIDTH/2 - LENGTH/64, color=(255, 255, 255), speed=-LENGTH/320)
+ball = Ball(radius=LENGTH/64, position_x=LENGTH/2 - LENGTH/64, position_y=WIDTH/2 - LENGTH/64, color=(255, 255, 255), speed=ball_direction*LENGTH/320)
+
+# Set up the scores
+player_score = Score(position_x=LENGTH/8, position_y=WIDTH/32, color=(255, 255, 255), font_size=LENGTH/16, font=None)
+enemy_score = Score(position_x=LENGTH - LENGTH/2 + LENGTH/8, position_y=WIDTH/32, color=(255, 255, 255), font_size=LENGTH/16, font=None)
 
 # Set up the enemy paddle direction
 enemy_paddle_direction = 1
@@ -121,6 +141,9 @@ while True:
             pygame.quit()
             exit()
 
+    # Draw the scores
+    player_score.draw(screen, "Player")
+    enemy_score.draw(screen, "Enemy")
 
     # Draw the player paddle
     player_paddle.draw(screen)
@@ -144,5 +167,7 @@ while True:
         ball.speed_x *= -1
 
     enemy_paddle.move(enemy_paddle_direction  * LENGTH/320)
+
+
 
     pygame.display.update()
